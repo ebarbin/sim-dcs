1cb8f5a3359db74120a0bb511eac36517704a56b
Emmanuel Barbin <emmanuel.barbin@gmail.com>
2020-12-12 14:04:07 -0300

    update

--- server.js
+++ server.js
@@ -28,28 +28,11 @@ app.get('/*', (req,res) => {
   res.sendFile(process.cwd()+"/app/dist/sim-dcs/index.html")
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+//app.listen(WEBSERVER_PORT,'0.0.0.0', () => { console.log('Server listening on the port ' + WEBSERVER_PORT) });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
-  }
-});
-
-client.login('Nzg2Mjg5MDUxNjk0MTM3MzQ1.X9EOvg.pbp1Gy4TwyuolLkHSLz3xx4N1mM').then((data)=>{
-  console.log(data);
-})
-console.log(client.channels.cache.get("444337434176520193"));
-app.listen(WEBSERVER_PORT,'0.0.0.0', () => { console.log('Server listening on the port ' + WEBSERVER_PORT) });
-
-/*mongoose.connect('mongodb://localhost:27017/sim-dcs', { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>{
+mongoose.connect('mongodb://localhost:27017/sim-dcs', { useNewUrlParser: true, useUnifiedTopology: true }).then(() =>{
   app.listen(WEBSERVER_PORT,'0.0.0.0', () => { console.log('Server listening on the port ' + WEBSERVER_PORT) });
 }).catch(err => {
   console.log(err);
-});*/
+});
 
